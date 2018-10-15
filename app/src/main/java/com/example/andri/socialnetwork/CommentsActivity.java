0a0528bb5f19c849e890217d7de5d34f5cc39e2c
AndriZZ <andri.ivanov@gmail.com>
2018-10-15 21:39:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/andri/socialnetwork/CommentsActivity.java
@@ -0,0 +1,122 @@
+package com.example.andri.socialnetwork;
+
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class CommentsActivity extends AppCompatActivity {
+        private ImageButton PostCommentButton;
+        private EditText CommentInputText;
+        private RecyclerView CommentsList;
+        private DatabaseReference UsersRef,PostsRef;
+        private String Post_Key, current_user_id;
+        private  FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+
+
+        Post_Key=getIntent().getExtras().get("PostKey").toString();
+        mAuth= FirebaseAuth.getInstance();
+        current_user_id= mAuth.getCurrentUser().getUid();
+
+        UsersRef= FirebaseDatabase.getInstance().getReference().child("Users");
+        PostsRef = FirebaseDatabase.getInstance().getReference().child("Posts").child(Post_Key).child("Comments");
+
+        CommentsList=(RecyclerView) findViewById(R.id.comments_list);
+        CommentsList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        CommentsList.setLayoutManager(linearLayoutManager );
+
+        CommentInputText =(EditText) findViewById(R.id.comment_input);
+        PostCommentButton = (ImageButton) findViewById(R.id.post_comment_btn);
+
+        PostCommentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UsersRef.child(current_user_id).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()){
+                            String userName= dataSnapshot.child("username").getValue().toString();
+
+                            ValidateComment(userName);
+
+                            CommentInputText.setText("");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void ValidateComment(String userName) {
+
+        String commentText = CommentInputText.getText().toString();
+        if(TextUtils.isEmpty(commentText)){
+            Toast.makeText(this, "Please insert text to comment..", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Calendar calFordDate = Calendar.getInstance();
+            SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-yyyy");
+           final String saveCurrentDate = currentDate.format(calFordDate.getTime());
+            Calendar calFordTime = Calendar.getInstance();
+
+            SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm");
+            final String saveCurrentTime = currentTime.format(calFordDate.getTime());
+
+            final String RandomKey = current_user_id + saveCurrentDate + saveCurrentTime;
+            HashMap commentsMap = new HashMap();
+                commentsMap.put("uid",current_user_id);
+                commentsMap.put("comment",commentText);
+                commentsMap.put("date",saveCurrentDate);
+                commentsMap.put("time",saveCurrentTime);
+                commentsMap.put("username",userName);
+
+                PostsRef.child(RandomKey).updateChildren(commentsMap)
+                        .addOnCompleteListener(new OnCompleteListener() {
+                            @Override
+                            public void onComplete(@NonNull Task task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(CommentsActivity.this, "You have commented successfully..", Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    Toast.makeText(CommentsActivity.this, "Error publishing comment..", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+        }
+    }
+}
